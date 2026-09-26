1eaad56f7b1b2dfad4c0a5eb6dc901dd4a6c67f9
hacasais <81494305+hacasais@users.noreply.github.com>
2026-09-26 09:00:50 +0100

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,22 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+    signingConfigs {
+        create("garagem") {
+            storeFile = file("garagem.keystore.jks")
+            storePassword = "garagem123"
+            keyAlias = "garagem"
+            keyPassword = "garagem123"
+        }
+    }
+    buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("garagem")
+        }
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("garagem")
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
